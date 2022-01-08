246de2af73a86c9f3adda749602a2c65113b13ce
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-01-08 12:19:15 +0000

    experiments in feature detection

--- src/cv.cpp
+++ src/cv.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <limits>
 #include <opencv2/core.hpp>
+#include <opencv2/core/hal/intrin.hpp>
+#include <opencv2/core/hal/intrin_sse.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/features2d.hpp>
@@ -16,7 +18,6 @@
 #include <vector>
 
 #include "tracking.hpp"
-
 
 struct Tim {
     std::chrono::steady_clock::time_point start =
@@ -31,12 +32,16 @@ struct Tim {
 };
 
 void FastHarris(const cv::Mat& gray) {
-    const int win_size = 5;
-    const int half_win_size = win_size / 2;
+    constexpr int win_size_pow2 = 4;
+    constexpr int win_size = 1 << win_size_pow2;
+    constexpr int half_win_size = win_size / 2;
     cv::Mat grad_x, grad_y;
 
+    {
+        Tim t;
     cv::Sobel(gray, grad_x, CV_16S, 1, 0);
     cv::Sobel(gray, grad_y, CV_16S, 0, 1);
+    }
 
     cv::Mat grad_xx, grad_xy, grad_yy;
 
@@ -45,91 +50,76 @@ void FastHarris(const cv::Mat& gray) {
     cv::multiply(grad_x, grad_y, grad_xy, k);
     cv::multiply(grad_y, grad_y, grad_yy, k);
 
-    double min, max;
-    cv::minMaxLoc(grad_xx, &min, &max);
-    std::cout << min << " " << max << std::endl;
+    // double min, max;
+    // cv::minMaxLoc(grad_xx, &min, &max);
+    // std::cout << min << " " << max << std::endl;
 
     {
-    Tim t;
-    for (int i = 0; i < grad_xx.rows; ++i) {
-        int32_t prev_xx{0}, prev_xy{0}, prev_yy{0};
-        for (int j = 1; j < grad_xx.cols; ++j) {
-            prev_xx = (prev_xx + grad_xx.at<int16_t>(i, j));
-            prev_xy = (prev_xy + grad_xy.at<int16_t>(i, j));
-            prev_yy = (prev_yy + grad_yy.at<int16_t>(i, j));
-            grad_xx.at<int16_t>(i, j) = prev_xx / win_size;
-            grad_xy.at<int16_t>(i, j) = prev_xy / win_size;
-            grad_yy.at<int16_t>(i, j) = prev_yy / win_size;
+        Tim t;
+        for (int i = 0; i < grad_xx.rows; ++i) {
+            int32_t prev_xx{0}, prev_xy{0}, prev_yy{0};
+            int16_t* row_xx = reinterpret_cast<int16_t*>(grad_xx.ptr(i));
+            int16_t* row_xy = reinterpret_cast<int16_t*>(grad_xy.ptr(i));
+            int16_t* row_yy = reinterpret_cast<int16_t*>(grad_yy.ptr(i));
+            for (int j = 1; j < grad_xx.cols; ++j) {
+                prev_xx = (prev_xx + row_xx[j]);
+                prev_xy = (prev_xy + row_xy[j]);
+                prev_yy = (prev_yy + row_yy[j]);
+                row_xx[j] = prev_xx >> 2 * win_size_pow2;
+                row_xy[j] = prev_xy >> 2 * win_size_pow2;
+                row_yy[j] = prev_yy >> 2 * win_size_pow2;
+            }
         }
-    }
     }
 
     {
-    Tim t;
-    for (int j = 0; j < grad_xx.cols; ++j) {
-        int32_t prev_xx{0}, prev_xy{0}, prev_yy{0};
-        for (int i = 1; i < grad_xx.rows; ++i) {
-            prev_xx = (prev_xx + grad_xx.at<int16_t>(i, j));
-            prev_xy = (prev_xy + grad_xy.at<int16_t>(i, j));
-            prev_yy = (prev_yy + grad_yy.at<int16_t>(i, j));
-            grad_xx.at<int16_t>(i, j) = prev_xx / win_size;
-            grad_xy.at<int16_t>(i, j) = prev_xy / win_size;
-            grad_yy.at<int16_t>(i, j) = prev_yy / win_size;
+        Tim t;
+        for (int j = 0; j < grad_xx.cols; j += 8) {
+            cv::v_int16x8 prev_xx = cv::v_setzero_s16(), prev_xy = cv::v_setzero_s16(), prev_yy = cv::v_setzero_s16();
+            for (int i = 1; i < grad_xx.rows; ++i) {
+                int16_t* row_xx = reinterpret_cast<int16_t*>(grad_xx.ptr(i));
+                int16_t* row_xy = reinterpret_cast<int16_t*>(grad_xy.ptr(i));
+                int16_t* row_yy = reinterpret_cast<int16_t*>(grad_yy.ptr(i));
+                prev_xx = cv::v_add_wrap(prev_xx, cv::v_load(row_xx + j));
+                prev_xy = cv::v_add_wrap(prev_xy, cv::v_load(row_xy + j));
+                prev_yy = cv::v_add_wrap(prev_yy, cv::v_load(row_yy + j));
+                cv::v_store(row_xx + j, prev_xx);
+                cv::v_store(row_xy + j, prev_xy);
+                cv::v_store(row_yy + j, prev_yy);
+            }
         }
-    }
     }
 
     // cv::Mat img;
     // cv::convertScaleAbs(grad_xx, img, 1 / 256. / 4);
     // cv::imwrite("out1.png", img);
 
-    cv::Mat wgrad_xx = cv::Mat(grad_xx.rows, grad_xx.cols, CV_16S),
-            wgrad_xy = cv::Mat(grad_xy.rows, grad_xy.cols, CV_16S),
-            wgrad_yy = cv::Mat(grad_yy.rows, grad_yy.cols, CV_16S),
-            response = cv::Mat::zeros(grad_yy.rows, grad_yy.cols, CV_16S);
+    cv::Mat response = cv::Mat::zeros(grad_yy.rows, grad_yy.cols, CV_16S);
     {
-    Tim t;
-    for (int i = win_size; i < grad_xx.rows; ++i) {
-        for (int j = win_size; j < grad_xx.cols; ++j) {
-            int m0 = wgrad_xx.at<int16_t>(i, j) =
-                grad_xx.at<uint16_t>(i, j) -
-                grad_xx.at<uint16_t>(i, j - win_size) -
-                grad_xx.at<uint16_t>(i - win_size, j) +
-                grad_xx.at<uint16_t>(i - win_size, j - win_size);
-            int m1 = wgrad_xy.at<int16_t>(i, j) =
-                grad_xy.at<uint16_t>(i, j) -
-                grad_xy.at<uint16_t>(i, j - win_size) -
-                grad_xy.at<uint16_t>(i - win_size, j) +
-                grad_xy.at<uint16_t>(i - win_size, j - win_size);
-            int m2 = wgrad_yy.at<int16_t>(i, j) =
-                grad_yy.at<uint16_t>(i, j) -
-                grad_yy.at<uint16_t>(i, j - win_size) -
-                grad_yy.at<uint16_t>(i - win_size, j) +
-                grad_yy.at<uint16_t>(i - win_size, j - win_size);
+        Tim t;
+        for (int i = win_size; i < grad_xx.rows; ++i) {
+            uint16_t* row_xx = reinterpret_cast<uint16_t*>(grad_xx.ptr(i));
+            uint16_t* row_xy = reinterpret_cast<uint16_t*>(grad_xy.ptr(i));
+            uint16_t* row_yy = reinterpret_cast<uint16_t*>(grad_yy.ptr(i));
+            uint16_t* mrow_xx = reinterpret_cast<uint16_t*>(grad_xx.ptr(i - win_size));
+            uint16_t* mrow_xy = reinterpret_cast<uint16_t*>(grad_xy.ptr(i - win_size));
+            uint16_t* mrow_yy = reinterpret_cast<uint16_t*>(grad_yy.ptr(i - win_size));
+            for (int j = win_size; j < grad_xx.cols; ++j) {
+                int m0 = static_cast<int16_t>(row_xx[j] - row_xx[j - win_size] - mrow_xx[j] + mrow_xx[j - win_size]);
+                int m1 = static_cast<int16_t>(row_xy[j] - row_xy[j - win_size] - mrow_xy[j] + mrow_xy[j - win_size]);
+                int m2 = static_cast<int16_t>(row_yy[j] - row_yy[j - win_size] - mrow_yy[j] + mrow_yy[j - win_size]);
 
-            response.at<int16_t>(i - half_win_size, j - half_win_size) =
-                std::max(
-                    (100 * (m0 * m2 - m1 * m1) - 5 * (m0 + m2) * (m0 + m2)) /
-                        100 / 256,
-                    0);
+                response.at<int16_t>(i - half_win_size, j - half_win_size) = 
+                    std::max((100 * (m0 * m2 - m1 * m1) -
+                              5 * (m0 + m2) * (m0 + m2)) /
+                                 100 / 256,
+                             0);
+            }
         }
     }
-    }
 
-    cv::minMaxLoc(response, &min, &max);
-    std::cout << min << " " << max << std::endl;
-
-    cv::Mat img;
-    cv::convertScaleAbs(response, img, 1);
-    cv::imwrite("out2.png", img);
-
-    // auto m0 = cv::sum(grad_x.mul(grad_x))[0];
-    // auto m1 = cv::sum(grad_x.mul(grad_y))[0];
-    // auto m2 = cv::sum(grad_y.mul(grad_y))[0];
-
-    // const auto k = .05;
-    // auto response = m0 * m2 - m1 * m1 - k * (m0 + m2) * (m0 + m2);
-    // return std::max(response, 0.);
+    // cv::minMaxLoc(response, &min, &max);
+    // std::cout << min << " " << max << std::endl;
 }
 
 int main() {
@@ -145,16 +135,21 @@ int main() {
     cap.read(img);
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 
-    auto start = std::chrono::steady_clock::now();
+    {
+    Tim t;
+    for (int i = 0; i < 30; ++i) {
+        FastHarris(gray);
+    }
+    }
 
-    FastHarris(gray);
+    {
+    Tim t;
+    for (int i = 0; i < 30; ++i) {
+        std::vector<cv::Point> pts;
+        cv::goodFeaturesToTrack(gray, pts, 500, .01, 0);
+    }
+    }
 
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> dur = (end - start);
-    std::cout
-        << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count()
-        << std::endl;
 
     return 0;
 }
-
