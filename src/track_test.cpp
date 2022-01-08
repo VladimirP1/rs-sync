8af972b0dd502cebfd24083d41aac7b47ee54ac9
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-01-08 14:07:16 +0000

    tracking+undistort

--- src/track_test.cpp
+++ src/track_test.cpp
@@ -1,8 +1,11 @@
 
+#include <opencv2/core/hal/interface.h>
+
 #include <chrono>
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
@@ -33,25 +36,17 @@ class VideoReader {
         return success;
     }
 
-    const cv::Mat& Prev() const {
-        return prev_frame_;
-    }
+    const cv::Mat& Prev() const { return prev_frame_; }
 
-    const cv::Mat& Cur() const {
-        return cur_frame_;
-    }
+    const cv::Mat& Cur() const { return cur_frame_; }
 
-    const cv::Mat& PrevGray() const {
-        return prev_gray_;
-    }
+    const cv::Mat& PrevGray() const { return prev_gray_; }
 
-    const cv::Mat& CurGray() const {
-        return cur_gray_;
-    }
+    const cv::Mat& CurGray() const { return cur_gray_; }
 
    private:
-   cv::Mat cur_gray_;
-   cv::Mat prev_gray_;
+    cv::Mat cur_gray_;
+    cv::Mat prev_gray_;
     cv::Mat cur_frame_;
     cv::Mat prev_frame_;
     cv::VideoCapture cap_;
@@ -61,12 +56,11 @@ int main() {
     cv::VideoCapture cap("141101AA.MP4");
     // cv::VideoCapture cap("193653AA.MP4");
 
-    cap.set(cv::CAP_PROP_POS_MSEC, 42e3);
+    cap.set(cv::CAP_PROP_POS_MSEC, 50e3);
 
     VideoReader reader("141101AA.MP4");
     TrackerImpl tracker(30, 700);
     FisheyeCalibration calibration;
-
 
     std::ifstream("GoPro_Hero6_2160p_43.json") >> calibration;
 
@@ -76,12 +70,39 @@ int main() {
 
     auto start = std::chrono::steady_clock::now();
 
-    cv::Mat img;
-    for (int i = 1; i < 300; ++i) {
-        reader.Advance();
-        auto [old_c, new_c] = tracker.Track(reader.PrevGray(), reader.CurGray());
+    ////
+    cv::Mat P;
+    cv::fisheye::estimateNewCameraMatrixForUndistortRectify(
+        calibration.CameraMatrix(), calibration.DistortionCoeffs(),
+        cv::Size(4000, 3000), cv::Mat::eye(3, 3, CV_32F), P);
 
-        // img = reader.Cur().clone();
+    ////
+
+    cv::Mat img;
+    for (int i = 1; i < 30; ++i) {
+        reader.Advance();
+        auto [old_c, new_c] =
+            tracker.Track(reader.PrevGray(), reader.CurGray());
+        std::vector<cv::Point2f> old_u, new_u;
+
+        cv::fisheye::undistortPoints(old_c, old_u, calibration.CameraMatrix(),
+                                     calibration.DistortionCoeffs(), cv::Mat::eye(3,3,CV_32F), P);
+        cv::fisheye::undistortPoints(new_c, new_u, calibration.CameraMatrix(),
+                                     calibration.DistortionCoeffs(), cv::Mat::eye(3,3,CV_32F), P);
+
+        img = reader.Cur().clone();
+
+        cv::Mat out;
+        cv::fisheye::undistortImage(img, out, calibration.CameraMatrix(),
+                                    calibration.DistortionCoeffs(), P,
+                                    cv::Size(4000, 3000));
+
+   
+
+        for (int i = 0; i < old_c.size(); ++i) {
+            cv::line(img, old_u[i], new_u[i], cv::Scalar(0, 0, 255), 3);
+        }
+        cv::imwrite("outz" + std::to_string(i) + ".jpg", img);
 
         // for (int i = 0; i < old_c.size(); ++i) {
         //     cv::line(img, old_c[i], new_c[i], cv::Scalar(0, 0, 255), 3);
@@ -94,6 +115,22 @@ int main() {
     std::cout
         << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count()
         << std::endl;
+
+    std::vector<cv::Point2f> old_c, old_u;
+    old_c.push_back({4000, 3000});
+    cv::fisheye::undistortPoints(old_c, old_u, calibration.CameraMatrix(),
+                                 calibration.DistortionCoeffs());
+    std::cout << old_u[0] << std::endl;
+
+    img = reader.Cur();
+    cv::Mat out;
+    cv::fisheye::undistortImage(img, out, calibration.CameraMatrix(),
+                                calibration.DistortionCoeffs(), P,
+                                cv::Size(4000, 3000));
+
+    std::cout << P << std::endl;
+
+    cv::imwrite("out.png", out);
 
     return 0;
 }
