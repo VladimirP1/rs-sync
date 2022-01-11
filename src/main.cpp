4ead29864e5d9d53ec509ef5f1a8df9f60aff9fc
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-01-11 15:46:55 +0000

    self-correlation working

--- src/main.cpp
+++ src/main.cpp
@@ -138,6 +138,10 @@ class Mosaic {
 
         m.copyTo(out(cv::Rect(j, i, patch_size_, patch_size_)));
 
+        return true;
+    }
+
+    bool Advance() {
         j += patch_size_;
         if (j + patch_size_ >= cols_) {
             j = 0;
@@ -147,6 +151,7 @@ class Mosaic {
         if (i + patch_size_ >= rows_) {
             return false;
         }
+        return true;
     }
 
    private:
@@ -188,53 +193,66 @@ int main(int argc, char** argv) {
 
     std::cout << "Found " << corners.size() << " corners" << std::endl;
 
-    double patch_size = 40;
-    double dst_patch_size = 40;
-    cv::Mat result;
-    cv::Mat mos = cv::Mat::zeros(1080, 1920, CV_8UC3);
-    Mosaic mosaic(mos, dst_patch_size * 2);
+    double patch_size = 15;
+    double dst_patch_size = 10;
+    cv::Mat corr, result;
+    cv::Mat corr_mos = cv::Mat::zeros(800, 800, frame.type());
+    cv::Mat patch_mos = cv::Mat::zeros(800, 800, frame.type());
+    Mosaic mosaic(corr_mos, dst_patch_size * 2);
     cv::Mat_<double> A, PS = P.clone();
     PS(0, 2) = PS(1, 2) = 0.;
     {
-    Tim t;
-    for (int i = 0; i < corners.size(); ++i) {
-        cv::Point2d center = corners[i];
+        Tim t;
+        for (int i = 0; i < corners.size(); ++i) {
+            cv::Point2d center = corners[i];
 
-        if (center.x + patch_size > frame.cols ||
-            center.y + patch_size > frame.rows || center.x - patch_size < 0 ||
-            center.y - patch_size < 0) {
-            continue;
+            if (center.x + patch_size > frame.cols ||
+                center.y + patch_size > frame.rows ||
+                center.x - patch_size < 0 || center.y - patch_size < 0) {
+                continue;
+            }
+
+            cv::Mat patch =
+                frame(cv::Rect(center - cv::Point2d{patch_size, patch_size},
+                               cv::Size(patch_size * 2, patch_size * 2)));
+
+            cv::Point2d xy;
+            UndistortPointG(center, xy, A, c.CameraMatrix(),
+                            c.DistortionCoeffs());
+
+            cv::Mat_<double> T = (PS * A);
+
+            cv::Mat_<double> cp(3, 1, CV_64F);
+            cp << patch_size, patch_size, 1;
+
+            T.col(2) = -T * cp;
+            T(0, 2) += dst_patch_size;
+            T(1, 2) += dst_patch_size;
+
+            cv::warpAffine(patch, result, T(cv::Rect(0, 0, 3, 2)),
+                           cv::Size(dst_patch_size * 2, dst_patch_size * 2),
+                           cv::INTER_CUBIC, cv::BORDER_CONSTANT,
+                           cv::Scalar(0, 255, 0));
+
+            mosaic.Add(patch_mos, result);
+
+            const double filter_size = 4;
+            result.convertTo(corr, CV_32FC3);
+            cv::Mat templ = corr(cv::Rect(
+                dst_patch_size - filter_size, dst_patch_size - filter_size,
+                2 * filter_size, 2 * filter_size));
+
+            cv::matchTemplate(corr, templ, corr, cv::TM_SQDIFF_NORMED);
+            corr.convertTo(corr, CV_8UC1, 255);
+            cv::cvtColor(corr, corr, cv::COLOR_GRAY2BGR);
+
+            result(cv::Rect(cv::Point(0, 0), result.size())) = cv::Scalar(0,0,0);
+            corr.copyTo(result(cv::Rect(cv::Point(0, 0), corr.size())));
+
+            mosaic.Add(corr_mos, result);
+            mosaic.Advance();
         }
-
-        cv::Mat patch =
-            frame(cv::Rect(center - cv::Point2d{patch_size, patch_size},
-                           cv::Size(patch_size * 2, patch_size * 2)));
-
-        cv::Point2d xy;
-        UndistortPointG(center, xy, A, c.CameraMatrix(), c.DistortionCoeffs());
-
-        // if ((std::abs(A(0,0)) + std::abs(A(1,1))) > 15*(std::abs(A(0,1)) +
-        // std::abs(A(1,0)))) {
-        //     continue;
-        // }
-        cv::Mat_<double> T = (PS * A);
-
-        cv::Mat_<double> cp(3, 1, CV_64F);
-        cp << patch_size, patch_size, 1;
-
-        T.col(2) = -T * cp;
-        T(0, 2) += dst_patch_size;
-        T(1, 2) += dst_patch_size;
-
-        cv::warpAffine(patch, result, T(cv::Rect(0, 0, 3, 2)),
-                       cv::Size(dst_patch_size * 2, dst_patch_size * 2),
-                       cv::INTER_CUBIC, cv::BORDER_CONSTANT,
-                       cv::Scalar(0, 255, 0));
-
-        mosaic.Add(mos, result);
-        // cv::imwrite("outa.png", patch);
-        // cv::imwrite("outb.png", result);
     }
-    }
-    cv::imwrite("out.png", mos);
+    cv::imwrite("out_corr.png", corr_mos);
+    cv::imwrite("out_patch.png", patch_mos);
 }
