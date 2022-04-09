68262cfd366d608fe755ac8e6d77feb36be658a2
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-04-09 18:00:04 +0000

    generate some graphics for thesis

---
+++ python/plot_normals.py
@@ -0,0 +1,62 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random import default_rng
+from scipy.optimize import least_squares
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from scipy.spatial.transform import Rotation
+
+plt.rcParams.update({
+    "pgf.texsystem": "pdflatex"
+})
+
+frame = 600
+
+dirs = np.genfromtxt('dump/motion.csv', delimiter=',')
+m = np.genfromtxt('dump/normals_{}.csv'.format(frame), delimiter=',')
+normal = dirs[dirs[:,0] == frame][0, 1:]
+
+
+fig = plt.figure(figsize=(7,7))
+ax = fig.add_subplot(projection='3d')
+ax.view_init(-14,-24)
+
+ax.scatter(m[:,0], m[:,1], m[:,2], c = np.abs(np.dot(m, normal)), cmap='jet')
+
+# bounding box
+minv,maxv = np.min(m), np.max(m)
+rad = (maxv - minv) / 2
+center = ((np.min(m[:,0]) + np.max(m[:,0])) / 2, (np.min(m[:,1]) + np.max(m[:,1])) / 2, (np.min(m[:,2]) + np.max(m[:,2])) / 2)
+ax.set_xlim((center[0] - rad , center[0] + rad))
+ax.set_ylim((center[1] - rad , center[1] + rad))
+ax.set_zlim((center[2] - rad , center[2] + rad))
+
+#
+axis = np.cross((0,0,1), normal)
+angle = np.arcsin(np.linalg.norm(axis)) + np.pi/2
+rot = Rotation.from_rotvec(axis / np.linalg.norm(axis) * angle)
+
+#
+circle = np.array([np.array((np.sin(x), np.cos(x), 0)) for x in np.linspace(0,2*np.pi,100)])
+circle = rot.apply(circle) * rad
+circle += center - np.dot(center, normal) * normal
+
+verts = [list(zip(circle[:,0], circle[:,1], circle[:,2]))]
+srf = Poly3DCollection(verts, alpha=.25, facecolor='orange')
+plt.gca().add_collection3d(srf)
+
+qlen = rad / 2
+ax.quiver([0],[0],[0],[qlen],[0],[0], colors='r')
+ax.quiver([0],[0],[0],[0],[qlen],[0], colors='g')
+ax.quiver([0],[0],[0],[0],[0],[qlen], colors='b')
+
+# ax.scatter(m[:,0], m[:,1], m[:,2], c=weights, cmap='jet')
+# fig.colorbar(plt.cm.ScalarMappable(cmap = 'jet'), ax = ax)
+
+fig.tight_layout()
+
+plt.show()
+
+print('azim =', ax.azim, ', elev =', ax.elev)
+
+plt.savefig('fig.pgf', backend='pgf')
---
+++ src/problem_dump.cpp
@@ -0,0 +1,122 @@
+#include <execution>
+#include <iomanip>
+#include <iostream>
+#include <mutex>
+#include <random>
+#include <vector>
+#include <filesystem>
+
+#include "cv.hpp"
+#include "ndspline.hpp"
+#include "quat.hpp"
+#include "signal.hpp"
+#include "backtrack.hpp"
+#include "simple_calculus.hpp"
+#include "inline_utils.hpp"
+#include "gsl.hpp"
+
+#include <telemetry-parser.h>
+
+struct OptData {
+    double quats_start{};
+    int sample_rate{};
+    ndspline quats{};
+
+    FramesFlow flows{};
+};
+
+void optdata_fill_gyro(OptData& optdata, const char* filename, const char* orient) {
+    tp_gyrodata data = tp_load_gyro(filename, orient);
+    arma::mat timestamps(data.timestamps, 1, data.samples);
+    arma::mat gyro(data.gyro, 3, data.samples);
+    tp_free(data);
+    optdata.sample_rate = gyro_interpolate(timestamps, gyro);
+
+    arma::mat quats(4, data.samples);
+    quats.col(0) = {1, 0, 0, 0};
+    for (int i = 1; i < quats.n_cols; ++i) {
+        quats.col(i) = arma::normalise(
+            quat_prod(quats.col(i - 1), quat_from_aa(gyro.col(i) / optdata.sample_rate)));
+    }
+    optdata.quats_start = timestamps.front();
+    optdata.quats = ndspline::make(quats);
+}
+
+void opt_compute_problem(int frame, double gyro_delay, const OptData& data, arma::mat& problem) {
+    const auto& flow = data.flows.at(frame);
+    gyro_delay /= 1000;
+
+    arma::mat ap = flow.rows(0, 2);
+    arma::mat bp = flow.rows(3, 5);
+    arma::mat at = (flow.row(6) - data.quats_start + gyro_delay) * data.sample_rate;
+    arma::mat bt = (flow.row(7) - data.quats_start + gyro_delay) * data.sample_rate;
+
+    problem.resize(at.n_cols, 3);
+    for (int i = 0; i < at.n_cols; ++i) {
+        arma::vec4 a_conj = quat_conj(data.quats.eval(at[i]));
+        arma::vec4 b = data.quats.eval(bt[i]);
+        double inv_ab_norm = (1. / arma::norm(a_conj)) * (1. / arma::norm(b));
+        arma::vec4 rot = quat_prod(a_conj, b) * inv_ab_norm;
+        arma::vec3 br = quat_rotate_point(quat_conj(rot), bp.col(i));
+        problem.row(i) = arma::trans(arma::cross(ap.col(i), br));
+    }
+}
+
+arma::vec3 opt_guess_translational_motion(const arma::mat& problem) {
+    arma::mat nproblem = problem;
+    nproblem.each_row([](arma::mat& m) { m = safe_normalize(m); });
+
+    arma::vec3 best_sol;
+    double least_med = std::numeric_limits<double>::infinity();
+    for (int i = 0; i < 200; ++i) {
+        int vs[3];
+        vs[0] = vs[1] = mtrand(0, problem.n_rows - 1);
+        while (vs[1] == vs[0]) vs[2] = vs[1] = mtrand(0, problem.n_rows - 1);
+        while (vs[2] == vs[1] || vs[2] == vs[0]) vs[2] = mtrand(0, problem.n_rows - 1);
+
+        arma::mat v = arma::trans(safe_normalize(arma::cross(
+            problem.row(vs[0]) - problem.row(vs[1]), problem.row(vs[0]) - problem.row(vs[2]))));
+
+        arma::mat residuals = nproblem * v;
+        arma::mat residuals2 = residuals % residuals;
+
+        std::sort(residuals2.begin(), residuals2.end());
+        double med = residuals2(residuals2.n_rows / 4, 0);
+        if (med < least_med) {
+            least_med = med;
+            best_sol = v;
+        }
+    }
+    return best_sol;
+}
+
+int main() {
+    std::cout << std::fixed << std::setprecision(14);
+
+    auto frame_start = 600, frame_end = 700;
+    double gyro_delay = -42;
+    // double gyro_delay = 1000;
+
+    OptData opt_data;
+    optdata_fill_gyro(opt_data, "GX011338.MP4", "yZX");
+    Lens lens = lens_load("lens.txt", "hero6_27k_43");
+    track_frames(opt_data.flows, lens, "GX011338.MP4", frame_start, frame_end);
+
+    std::filesystem::create_directories("dump");
+
+    std::ofstream motion_stream("dump/motion.csv");
+
+    arma::mat problem;
+    for (int frame = frame_start; frame < frame_end; ++frame) {
+        opt_compute_problem(frame, gyro_delay, opt_data, problem);
+
+        std::ofstream normals_stream("dump/normals_" + std::to_string(frame) + ".csv");
+        for (int i = 0; i < problem.n_rows; ++i) {
+            auto row = problem.row(i).eval();
+            normals_stream << row(0) << "," << row(1) << "," << row(2) << "\n";
+        }
+
+        auto motion = opt_guess_translational_motion(problem);
+        motion_stream << frame << "," << motion(0) << "," << motion(1) << "," << motion(2) << "\n";
+    }
+}
